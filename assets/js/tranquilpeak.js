47f64333f4ebfa93f3551032191a660e76be72d4
oe <okarl@outlook.com>
2021-11-02 05:06:59 +0000

    oe published a site update

--- assets/js/tranquilpeak.js
+++ assets/js/tranquilpeak.js
@@ -548,6 +548,22 @@
       });
     }
 
+    $('.post-content').each(function (i) {
+      $(this).find('img').each(function () {
+        if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
+
+        var alt = this.alt;
+
+        if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+
+        $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
+      });
+
+      $(this).find('.fancybox').each(function () {
+        $(this).attr('rel', 'article' + i);
+      });
+    });
+
     fancyFox();
 
     $(window).smartresize(function() {
